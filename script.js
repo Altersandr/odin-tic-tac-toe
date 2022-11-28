0da6d12e946f9ed8e0558a6c7b03d5d0c34ea06e
Oleks <archer.opendub@gmail.com>
2022-11-28 22:42:01 +0100

    made the rendering of the cells function and the click event on cells

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
-const Gameboard = (function(){
-    "use strict";
-    const theBoard = [["X", "O", "X"],["X", "O", "X"],["X", "O", "X"]];
-    console.log(theBoard[0][2])
+const Gameboard = {
     
-})();
+    board: ["x", "x", "x", "x", "x", "x", "x", "x", "x"],
+    
+    renderBoard: function (){
+        this.board.forEach(cell=>{
+            for (let i = 1; i < 10 ;i++){
+            const cell = document.querySelector(`.cell${i}`);
+            cell.innerHTML = this.board[i-1]
+            }})},
+            
+    clickEvent: function (){
+        const cells = document.querySelectorAll('.cells');
+        cells.forEach(cell=>{
+            cell.addEventListener('click', function(e){
+            e.target.innerHTML = "O";
+            })})},
+    
+    gameOn: function(){
+        this.renderBoard()
+        this.clickEvent()
+    },
 
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+//make gameboard object
+
+//make players object
+//player object has a name and a score
+
+//make game object
+
+//make display object
+
+
+
+
+
+
+Gameboard.gameOn()
